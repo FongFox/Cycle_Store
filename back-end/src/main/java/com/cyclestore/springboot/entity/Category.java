0cb17748312f261656f04be0f807c0b8bb57386d
FongFox <107380312+FongFox@users.noreply.github.com>
2023-09-22 21:15:03 +0700

    Set up spring boot initializr, update datebase design

---
+++ back-end/src/main/java/com/cyclestore/springboot/entity/Category.java
@@ -0,0 +1,53 @@
+package com.cyclestore.springboot.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "category")
+public class Category {
+    /*
+     * properties
+     * entity relation
+     * constructor
+     * getters, setters
+     * method
+     * */
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+    @Column(name = "name")
+    private String name;
+
+    public Category() {
+    }
+
+    public Category(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Category{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
---
+++ back-end/src/main/java/com/cyclestore/springboot/entity/Product.java
@@ -0,0 +1,127 @@
+package com.cyclestore.springboot.entity;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "product")
+public class Product {
+    /*
+    * properties
+    * entity relation
+    * constructor
+    * getters, setters
+    * method
+    * */
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+    @Column(name = "sku", unique=true)
+    private String sku;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "image")
+    private String image;
+    @Column(name = "description")
+    private String description;
+    @Column(name = "price")
+    private BigDecimal price;
+    @Column(name = "stock")
+    private int stock;
+    @Column(name = "is_active")
+    private boolean isActive;
+
+    public Product() {
+    }
+
+    public Product(String sku, String name, String image, String description, BigDecimal price, int stock, boolean isActive) {
+        this.sku = sku;
+        this.name = name;
+        this.image = image;
+        this.description = description;
+        this.price = price;
+        this.stock = stock;
+        this.isActive = isActive;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getSku() {
+        return sku;
+    }
+
+    public void setSku(String sku) {
+        this.sku = sku;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public void setActive(boolean active) {
+        isActive = active;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", sku='" + sku + '\'' +
+                ", name='" + name + '\'' +
+                ", image='" + image + '\'' +
+                ", description='" + description + '\'' +
+                ", price=" + price +
+                ", stock=" + stock +
+                ", isActive=" + isActive +
+                '}';
+    }
+}
